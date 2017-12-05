bc5aa5c2b199e0d1f7a77aea4d2415a4633d21be
chrisdavetv <chrisdavetv@users.noreply.github.com>
2017-12-05 16:25:56 +0800

    comitting breakpoint class

---
+++ src/compile/compilersource/Breakpoint.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compile.compilersource;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+
+/**
+ *
+ * @author chris
+ */
+public class Breakpoint// implements Runnable
+{
+    //lkjhlkjh
+    private static Boolean stopExecution = false;
+    private myGrammarParser.StatementContext ctx;
+    private ArrayList<Integer> breaklineList;
+    
+    public static int checkBreakpoint(myGrammarParser.StatementContext ctx, ArrayList<Integer> breaklineList){
+        String line = ctx.getStart().getLine() + "";
+        System.out.println("breakline time: "+breaklineList.size());
+        for(int i = 0; i < breaklineList.size(); i++){ 
+            int breakPointLineNum = breaklineList.get(i);
+            System.out.println("checking a breakline at line: "+breakPointLineNum);
+            if(line.equals(breaklineList.get(i).toString())){
+                System.out.println("found a breakline at line: "+breakPointLineNum);
+                //System.out.println("executing: "+Breakpoint.executing());
+                
+                return breakPointLineNum;
+                
+                /*while(Breakpoint.executing()){
+                    try {
+                        System.out.println("About to sleep");
+                        Thread.sleep(100);
+                        System.out.println("Woke up");
+                    } catch (InterruptedException ex) {
+                        System.out.println("Breakpoint sleep interrupted: "+ex.getMessage());
+                    }
+
+                }*/
+            }   
+         }
+        
+        return -1;
+    }
+    
+    public Breakpoint(){
+    }
+
+    
+}
